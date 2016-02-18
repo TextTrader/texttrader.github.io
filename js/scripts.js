f44733603ef6ea7cacc253f202a29644a607039b
Alex Marshall <amhokies@gmail.com>
2016-02-18 01:05:38 -0500

    Finished search results page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function($) {
-	$(search_box).on({
+
+	// Clearing and re-populating the search box appropriately on focus/blur
+	$('search_box').on({
 		focus: function(event) {
         	if (event.target.value.startsWith('Search')) {
         		event.target.value = '';
@@ -13,32 +15,33 @@ $(document).ready(function($) {
     	}
     });
 
+	// One of the radio buttons are clicked to change the search type
     $('input[type=radio][name=search_type]').change(function(event) {
     	var search_type = this.value === 'title' ? 'title' : 'ISBN';
-    	$(search_box).val('Search by ' + search_type);
+    	$('search_box').val('Search by ' + search_type);
     });
 
+    // Login link is clicked, show login popup
     $('a#login').click(function(event) {
     	$('div#login_popup').css('display','block');
     	$('div#fade').css('display','block');
     });
 
+    // Register link is clicked, show register popup
     $('a#register').click(function(event) {
     	$('div#register_popup').css('display','block');
     	$('div#fade').css('display','block');    
     });
 
+    // Register close is clicked, close register popup
     $('a#register_close').click(function(event) {
     	$('div#register_popup').css('display','none');
     	$('div#fade').css('display','none');    
     });
 
+    // Login close is clicked, close login popup
     $('a#login_close').click(function(event) {
     	$('div#login_popup').css('display','none');
     	$('div#fade').css('display','none');    
-    });
-
-    $(search_button).click(function(event) {
-    	
     });
 });
